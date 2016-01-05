c9af6855990e43b0d54495f0cb3487f81c3d584e
frank-mac <goodogod@gmail.com>
2016-01-05 08:28:56 +0800

    update package.json: add start script.

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/dinbendang';
-
 module.exports = connectionString;
--- models/create_tables.js
+++ models/create_tables.js
@@ -2,10 +2,24 @@ var pg = require('pg');
 var connectionString = require('../config.js');
 
 //var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/';
-
 var client = new pg.Client(connectionString);
 client.connect();
 // use query to run SQL query.
+
+/*
+CREATE TABLE users(user_id SERIAL PRIMARY KEY,name VARCHAR(32) not null,password VARCHAR(32) not null,create_date TIMESTAMP not null,role INTEGER not null,money NUMERIC(12, 2),image VARCHAR(512));
+ALTER TABLE users ADD UNIQUE (name);
+CREATE TABLE organizations(organization_id SERIAL PRIMARY KEY,name VARCHAR(32) not null,create_date TIMESTAMP not null);
+ALTER TABLE organizations ADD UNIQUE (name);
+CREATE TABLE organization_creators(id SERIAL PRIMARY KEY,creator_id INTEGER REFERENCES users(user_id),organization_id INTEGER  REFERENCES organizations(organization_id));
+CREATE TABLE user_organization(id SERIAL PRIMARY KEY,user_id INTEGER REFERENCES users(user_id),organization_id INTEGER  REFERENCES organizations(organization_id));
+CREATE TABLE stores(store_id SERIAL PRIMARY KEY,name VARCHAR(64) NOT NULL,phone_number VARCHAR(64),create_date TIMESTAMP,image VARCHAR(512),min_spending NUMERIC(12, 2),note VARCHAR(255));
+ALTER TABLE stores ADD UNIQUE (name);
+CREATE TABLE products(product_id SERIAL PRIMARY KEY,store_id INTEGER REFERENCES stores(store_id),name VARCHAR(64),price NUMERIC(12, 2));
+CREATE TABLE comments(comment_id SERIAL PRIMARY KEY,product_id INTEGER REFERENCES products(product_id),comment_user_id INTEGER REFERENCES users(user_id),text TEXT,stars INTEGER,date TIMESTAMP,image VARCHAR(512));
+CREATE TABLE parties(party_id SERIAL PRIMARY KEY,organization_id INTEGER REFERENCES organizations(organization_id),creator_id INTEGER REFERENCES users(user_id),store_id INTEGER REFERENCES Stores(store_id),name VARCHAR(64),create_date TIMESTAMP,expired_date TIMESTAMP,ready BOOLEAN);
+CREATE TABLE orders(order_id SERIAL PRIMARY KEY,user_id INTEGER REFERENCES users(user_id),party_id INTEGER REFERENCES parties(party_id),product VARCHAR(64),price NUMERIC(12, 2),create_date TIMESTAMP,note VARCHAR(255));
+ */
 
 /* 
  * Create users table.
